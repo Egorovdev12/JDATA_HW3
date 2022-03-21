d8569535fe622e7b63b0f865d570dcfa7c4718fd
ILYA EGOROV <egorovcf@gmail.com>
2022-03-21 17:38:04 +0500

    Commit #2

--- src/main/java/ru/netology/jdatahw_3_1/repository/Repository.java
+++ src/main/java/ru/netology/jdatahw_3_1/repository/Repository.java
@@ -1,30 +1,40 @@
 package ru.netology.jdatahw_3_1.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Repository
 public class Repository {
 
     private String scriptName = "myScript.sql";
-
-    @Autowired
     private JdbcTemplate jdbcTemplate;
+    private String sqlScript;
+
+    public Repository (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.sqlScript = read(scriptName);
+    }
 
 
     public String getProduct(String name) {
-        String productName = jdbcTemplate.queryForObject(read(scriptName), (resultSet, i) -> (resultSet.getString("product_name")), name);
-        return productName;
+        //List<String> productName = jdbcTemplate.queryForList(sqlScript, String.class);\
+//        List<Map<String, Object>> products = jdbcTemplate.queryForList(sqlScript, new Object[]{name}, name);
+//        products.forEach( rowMap -> {
+//            String userName = (String) rowMap.get("product_name");
+//            System.out.println(userName);
+//        });
+//        return "gg";
     }
 
     private static String read(String scriptFileName) {
